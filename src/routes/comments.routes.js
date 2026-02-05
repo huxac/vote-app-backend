a636e86e47a9f8b6ed4425872e79a921e54672dc
Excalibur <excalibur@voteapp.com>
2026-02-05 23:54:44 +0300

    Fix: Correct import path in comments.routes.js

--- src/routes/comments.routes.js
+++ src/routes/comments.routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const commentsController = require('../controllers/comments.controller');
-const { authenticate } = require('../controllers/auth.controller');
+const authenticate = require('../middlewares/auth.middleware');
 
 // GET /api/v1/comments/:questionId
 router.get('/:questionId', authenticate, commentsController.getComments);
